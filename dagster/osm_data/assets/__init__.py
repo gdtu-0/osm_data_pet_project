ceb153b0aa31163129059f1b08e5b428881d3fd4
gdtu-0 <gdtu-0@outlook.com>
2024-06-17 23:24:31 +0300

    Removed obsolete debug assets

--- dagster/osm_data/assets/__init__.py
+++ dagster/osm_data/assets/__init__.py
@@ -1,7 +1,6 @@
-from .changeset_data import changeset_data
-from .changeset_headers import changeset_headers
-from .cities_coordinates import cities_coordinates
 from .dbt_assets import osm_data_dbt_assets
 
 # Store asset definitions
-ASSET_DEFINITIONS = [changeset_data, changeset_headers, cities_coordinates, osm_data_dbt_assets]
+ASSET_DEFINITIONS = [
+    osm_data_dbt_assets,
+]
