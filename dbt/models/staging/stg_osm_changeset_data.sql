50da2de2207aaed2f888c54afd6a4c8754d2d350
gdtu-0 <gdtu-0@outlook.com>
2024-06-18 16:06:06 +0300

    dbt staging incremental

--- dbt/models/staging/stg_osm_changeset_data.sql
+++ dbt/models/staging/stg_osm_changeset_data.sql
@@ -1,6 +1,19 @@
-with stg_osm_changeset_data as (
-	select *
-		from {{ source ('osm_staging_tables', 'osm_changeset_data') }}
-)
+{{
+	config(
+		materialized = 'incremental',
+		unique_key = 'changeset_id',
+		incremental_strategy = 'delete+insert'
+	)
+}}
 
-select * from stg_osm_changeset_data
+select	load_timestamp,
+		changeset_id,
+		action,
+		elem_type,
+		elem_id,
+		k,
+		v
+	from {{ source ('osm_staging_tables', 'osm_changeset_data') }}
+{% if is_incremental() %}
+	where load_timestamp > (select coalesce(max(load_timestamp), TO_TIMESTAMP('2000-01-01 00:00:00','YYYY-MM-DD HH:MI:SS')) from {{ this }})
+{% endif %}
--- dbt/models/staging/stg_osm_changeset_headers.sql
+++ dbt/models/staging/stg_osm_changeset_headers.sql
@@ -1,6 +1,20 @@
-with stg_osm_changeset_headers as (
-	select *
-		from {{ source ('osm_staging_tables', 'osm_changeset_headers') }}
-)
+{{
+	config(
+		materialized = 'incremental',
+		unique_key = 'changeset_id',
+		incremental_strategy = 'delete+insert'
+	)
+}}
 
-select * from stg_osm_changeset_headers
+select	load_timestamp,
+		location_name,
+		changeset_id,
+		closed_at,
+		uid,
+		username,
+		comment,
+		source
+	from {{ source ('osm_staging_tables', 'osm_changeset_headers') }}
+{% if is_incremental() %}
+	where load_timestamp > (select coalesce(max(load_timestamp), TO_TIMESTAMP('2000-01-01 00:00:00','YYYY-MM-DD HH:MI:SS')) from {{ this }})
+{% endif %}
