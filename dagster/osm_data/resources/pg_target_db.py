4ead2ddbd37d6b7fb99db255e50225a385be41a8
gdtu-0 <gdtu-0@outlook.com>
2024-06-23 11:38:05 +0300

    remove oobsolete functions

--- dagster/osm_data/resources/pg_target_db.py
+++ dagster/osm_data/resources/pg_target_db.py
@@ -45,81 +45,81 @@ class Target_PG_DB(ConfigurableResource):
     def table_exists(self, connection, table_name:str) -> bool:
         """Check if table exists in database"""
 
-        sql_str = f"SELECT EXISTS (\n\tSELECT FROM information_schema.tables\n\t\tWHERE table_name = \'{table_name}\'\n)"
+        sql_str = f"SELECT EXISTS (\n\tSELECT FROM information_schema.tables WHERE table_name = \'{table_name}\'\n);"
 
         exists = False
         with connection.cursor() as cursor:
             cursor.execute(sql_str)
             exists = cursor.fetchone()[0]
-        return(exists)      
+        return(exists)
 
 
-    @handle_connection
-    def table_lines(self, connection, table_name:str) -> int:
-        """Get number of table lines"""
+    # @handle_connection
+    # def table_lines(self, connection, table_name:str) -> int:
+    #     """Get number of table lines"""
 
-        sql_str = f"SELECT COUNT(*) FROM {table_name}"
-        num_lines = 0
-        with connection.cursor() as cursor:
-            cursor.execute(sql_str)
-            num_lines = cursor.fetchone()[0]
-        return(num_lines)
-
-
-    @handle_connection
-    def create_table(self, connection, table_name:str, columns:List[dict]) -> str:
-        """Create table"""
-
-        columns_str = ",\n\t".join(f'{name} {specs}' for name, specs in columns.items())
-        sql_str = f"CREATE TABLE {table_name} (\n\t{columns_str}\n)"
-        with connection.cursor() as cursor:
-            cursor.execute(sql_str)
-            connection.commit()
-        return(sql_str)
+    #     sql_str = f"SELECT COUNT(*) FROM {table_name}"
+    #     num_lines = 0
+    #     with connection.cursor() as cursor:
+    #         cursor.execute(sql_str)
+    #         num_lines = cursor.fetchone()[0]
+    #     return(num_lines)
 
 
-    @handle_connection
-    def insert_into_table(self, connection, table_name:str, columns:tuple, values:List[tuple]):
-        """Insert values into table"""
+    # @handle_connection
+    # def create_table(self, connection, table_name:str, columns:List[dict]) -> str:
+    #     """Create table"""
 
-        columns_str = ", ".join(name for name in columns)
-        sql_str = f"INSERT INTO {table_name}\n\t({columns_str})\n\t VALUES %s"
-        with connection.cursor() as cursor:
-            psycopg2.extras.execute_values (
-                cursor, sql_str, values, template=None, page_size=100)
-            connection.commit()
-
-
-    @handle_connection
-    def truncate_table(self, connection, table_name:str):
-        """Truncate table"""
-
-        sql_str = f"TRUNCATE TABLE {table_name}"
-        with connection.cursor() as cursor:
-            cursor.execute(sql_str)
-            connection.commit()
+    #     columns_str = ",\n\t".join(f'{name} {specs}' for name, specs in columns.items())
+    #     sql_str = f"CREATE TABLE {table_name} (\n\t{columns_str}\n)"
+    #     with connection.cursor() as cursor:
+    #         cursor.execute(sql_str)
+    #         connection.commit()
+    #     return(sql_str)
 
 
-    @handle_connection
-    def select_from_table(self, connection, table_name:str, columns:tuple, where:Optional[List[str]] = None) -> Optional[list]:          # TODO: A lot to be done here...
-        """Select statement (very basic functionality)"""
+    # @handle_connection
+    # def insert_into_table(self, connection, table_name:str, columns:tuple, values:List[tuple]):
+    #     """Insert values into table"""
 
-        columns_str = ",\n\t\t".join(name for name in columns)
-        if where is not None:
-            where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
-        else:
-            where_str = ''
-        sql_str = f"SELECT\t\t{columns_str}\n\tFROM {table_name}\n{where_str}\n;"
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            cursor.execute(sql_str)
-            responce = cursor.fetchall()
-            if not responce:
-                return None
-            else:
-                result = []
-                for row in responce:
-                    result.append(dict(row))
-                return(result)
+    #     columns_str = ", ".join(name for name in columns)
+    #     sql_str = f"INSERT INTO {table_name}\n\t({columns_str})\n\t VALUES %s"
+    #     with connection.cursor() as cursor:
+    #         psycopg2.extras.execute_values (
+    #             cursor, sql_str, values, template=None, page_size=100)
+    #         connection.commit()
+
+
+    # @handle_connection
+    # def truncate_table(self, connection, table_name:str):
+    #     """Truncate table"""
+
+    #     sql_str = f"TRUNCATE TABLE {table_name}"
+    #     with connection.cursor() as cursor:
+    #         cursor.execute(sql_str)
+    #         connection.commit()
+
+
+    # @handle_connection
+    # def select_from_table(self, connection, table_name:str, columns:tuple, where:Optional[List[str]] = None) -> Optional[list]:          # TODO: A lot to be done here...
+    #     """Select statement (very basic functionality)"""
+
+    #     columns_str = ",\n\t\t".join(name for name in columns)
+    #     if where is not None:
+    #         where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
+    #     else:
+    #         where_str = ''
+    #     sql_str = f"SELECT\t\t{columns_str}\n\tFROM {table_name}\n{where_str}\n;"
+    #     with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+    #         cursor.execute(sql_str)
+    #         responce = cursor.fetchall()
+    #         if not responce:
+    #             return None
+    #         else:
+    #             result = []
+    #             for row in responce:
+    #                 result.append(dict(row))
+    #             return(result)
     
 
     @handle_connection
@@ -157,24 +157,24 @@ class Target_PG_DB(ConfigurableResource):
             connection.commit()
 
 
-    @handle_connection
-    def delete_from_table(self, connection, table_name:str, where:List[str]):
-        """Delete statement"""
+    # @handle_connection
+    # def delete_from_table(self, connection, table_name:str, where:List[str]):
+    #     """Delete statement"""
 
-        where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
-        sql_str = f"DELETE FROM {table_name}\n{where_str}\n;"
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            cursor.execute(sql_str)
-            connection.commit()
+    #     where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
+    #     sql_str = f"DELETE FROM {table_name}\n{where_str}\n;"
+    #     with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+    #         cursor.execute(sql_str)
+    #         connection.commit()
     
 
-    @handle_connection
-    def update_table(self, connection, table_name:str, set:List[str], where:List[str]):
-        """Update statement"""
+    # @handle_connection
+    # def update_table(self, connection, table_name:str, set:List[str], where:List[str]):
+    #     """Update statement"""
 
-        set_str = '\tSET ' + ",\n\t\t".join(line for line in set)
-        where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
-        sql_str = f"UPDATE {table_name}\n{set_str}\n{where_str}\n;"
-        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-            cursor.execute(sql_str)
-            connection.commit()
+    #     set_str = '\tSET ' + ",\n\t\t".join(line for line in set)
+    #     where_str = '\tWHERE ' + ",\n\t\tAND ".join(line for line in where)
+    #     sql_str = f"UPDATE {table_name}\n{set_str}\n{where_str}\n;"
+    #     with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+    #         cursor.execute(sql_str)
+    #         connection.commit()
